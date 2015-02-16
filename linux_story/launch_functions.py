f1c1a8683286a7df7a582437baa1e72e67e08a21
carolineclark <caroline@kano.me>
2015-02-16 17:40:12 +0000

    Changed copyright, removed author line

--- linux_story/launch_functions.py
+++ linux_story/launch_functions.py
@@ -2,10 +2,9 @@
 
 # launch_functions.py
 #
-# Copyright (C) 2014 Kano Computing Ltd.
+# Copyright (C) 2014, 2015 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
 #
-# Author: Caroline Clark <caroline@kano.me>
 # Launch the different processes which show in the terminal
 
 import os
--- linux_story/paths.py
+++ linux_story/paths.py
@@ -2,7 +2,7 @@
 
 # paths.py
 #
-# Copyright (C) 2014 Kano Computing Ltd.
+# Copyright (C) 2014, 2015 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
 #
 
