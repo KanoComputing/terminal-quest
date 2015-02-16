d92b30cb764044e4c0d0e3bce473dfd1e8942430
carolineclark <caroline@kano.me>
2015-02-16 11:07:41 +0000

    Changed ls and the shell commands to replace ~ with /home/username/.linux-story

--- linux_story/commands_real.py
+++ linux_story/commands_real.py
@@ -12,7 +12,7 @@
 import os
 import subprocess
 
-from helper_functions import colour_file_dir, debugger
+from helper_functions import colour_file_dir, debugger, hidden_dir
 from kano.colours import colourize256
 
 
@@ -31,6 +31,7 @@ def ls(current_dir, tree, line=""):
     get_all_info = False
     new_lines = False
     args = ["ls"]
+    line = line.replace('~', hidden_dir)
 
     if line:
         args = line.split(" ")
@@ -59,6 +60,13 @@ def ls(current_dir, tree, line=""):
     orig_output, err = p.communicate()
     debugger("orig_output = {}".format(orig_output))
     debugger("err = {}".format(err))
+
+    # The error will need to be edited if it contains info about the edited
+    # filename
+    if err:
+        err = err.replace(hidden_dir, '~')
+        print err
+        return
 
     # need to folter output
     files = orig_output.split('\n')
@@ -147,7 +155,18 @@ def shell_command(current_dir, tree, line, command_word=""):
     if not real_loc:
         return False
 
+    possible_path = line.split(' ')[-1]
+
+    # TODO: very lazy.  Change
+    # If path starts with ~, replace ~ with the hidden dir
+    if possible_path.startswith('~'):
+        possible_path = turn_abs_path_to_real_loc(possible_path)
+        array = line.split(' ')[:-1]
+        array.append(possible_path)
+        line = ' '.join(array)
+
     args = line.split(" ")
+    print 'real_loc = {}'.format(real_loc)
     p = subprocess.Popen(args, cwd=real_loc,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
@@ -165,6 +184,33 @@ def shell_command(current_dir, tree, line, command_word=""):
 
     # should this return stdout?
     return True
+
+
+def turn_abs_path_to_real_loc(path):
+    return path.replace('~', hidden_dir)
+
+
+# This checks if the path is valid
+def check_real_loc(tree, path):
+    '''Takes a path with ~ and checks the path is consistent with the
+    saved file structure.
+    If so, replaces the ~ with the hidden_dir path (~/.linux-story) and
+    returns the path.
+    '''
+    folders = path.split('/')
+
+    # if user starts line with ~, take their path as an absolute path
+    if folders[0] == '~':
+
+        # Need to check that the path is valid
+        # Check the subsequent folder do indeed belong to the correct
+        # folders
+        for i in range(1, len(folders)):
+            if not folders[i] in tree.show_dirs(folders[i - 1]):
+                return None
+
+    path = path.replace('~', hidden_dir)
+    return path
 
 
 # Will be identical to touch
